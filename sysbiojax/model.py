d8dc04add73c74deb983406b49b2ad74ca85c053
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-04-30 11:37:03 +0200

    Updated adders and internals

--- sysbiojax/model.py
+++ sysbiojax/model.py
@@ -1,7 +1,7 @@
 from typing import Dict, List, Optional, Union
 from dotted_dict import DottedDict
 from pydantic import BaseModel, Field, PrivateAttr, validator
-from sympy import Expr, sympify, symbols
+from sympy import Expr, Symbol, sympify, symbols
 
 from .utils import odeprint
 
@@ -26,9 +26,10 @@ class Species(BaseModel):
     class Config:
         arbitrary_types_allowed = True
 
-    name: Expr
+    name: str
+    symbol: Expr
 
-    @validator("name", pre=True)
+    @validator("symbol", pre=True)
     def convert_string_species_to_sympy(cls, value):
         """Converts given strings of unit definitions into SymPy symbols"""
 
@@ -44,15 +45,49 @@ class ODE(BaseModel):
 
     species: Species
     equation: Expr
-    parameters: List[Parameter] = Field(default_factory=list)
+    parameters: Dict[Union[str, Expr], Parameter] = Field(default_factory=DottedDict)
 
     __model__: Optional["Model"] = PrivateAttr(default=None)
+
+    def __setattr__(self, name, value):
+        super().__setattr__(name, value)
+
+        if name == "__model__":
+            self.add_parameters_to_model()
+
+        return
 
     @validator("equation", pre=True)
     def converts_ode_to_sympy(cls, value):
         """Convertes a string"""
 
         return sympify(value)
+
+    def add_parameters_to_model(self):
+        """Adds parameters given by the ODE to the model and ODE
+
+        This step is necessary to ensure that the parameters are available
+        model-wide and not just within the ODE. Also, this step can only be
+        done upon addition of the model due to no given knowledge of the species.
+        """
+
+        if self.__model__ is None:
+            return None
+
+        for symbol in self.equation.free_symbols:
+            if str(symbol) in self.__model__.species:
+                # Skip species
+                continue
+            elif _parameter_exists(str(symbol), self.__model__.parameters):
+                # Assign parameter if it is already present in the model
+                self.parameters[str(symbol)] = self.__model__.parameters[str(symbol)]
+                continue
+
+            # Create a new one and add it to the model and ODE
+            parameter = Parameter(name=str(symbol))
+
+            self.parameters[str(symbol)] = parameter
+            self.__model__.parameters[str(symbol)] = parameter
 
     def add_parameter(
         self,
@@ -68,9 +103,9 @@ class ODE(BaseModel):
         )
 
         if self.__model__ and not _parameter_exists(name, self.__model__.parameters):
-            self.__model__.parameters[parameter.name] = parameter
+            self.__model__.parameters[name] = parameter
 
-        self.parameters.append(parameter)
+        self.parameters[name] = parameter
 
 
 class Model(BaseModel):
@@ -78,7 +113,7 @@ class Model(BaseModel):
         arbitrary_types_allowed = True
 
     name: str
-    odes: List[ODE] = Field(default_factory=list)
+    odes: Dict[str, ODE] = Field(default_factory=DottedDict)
     species: Dict[str, Species] = Field(default_factory=DottedDict)
     parameters: Dict[str, Parameter] = Field(default_factory=DottedDict)
 
@@ -100,51 +135,84 @@ class Model(BaseModel):
         self,
         equation: str,  # type: ignore
         species: str,
-        parameters: List[Union[str, Parameter]] = [],
+        species_map: Optional[Dict[str, str]] = None,
     ):  # type: ignore
         """Adds a an ODE to the model"""
 
-        if any(str(ode.species.name) == species for ode in self.odes):
+        if any(str(ode.species.name) == species for ode in self.odes.values()):
             raise ValueError(f"Species {species} already exists in model")
 
         if isinstance(equation, str):
             equation: Expr = sympify(equation)
 
-        # Turn all parameters into Parameter objects
-        converted_params: List[Parameter] = [
-            parameter if isinstance(parameter, Parameter) else Parameter(name=parameter)
-            for parameter in parameters
-        ]
-
-        # Add parameters to model
-        for parameter in converted_params:
-            if not isinstance(parameter, str) and not _parameter_exists(
-                parameter.name, self.parameters
-            ):
-                self.parameters[parameter.name] = parameter
-
         # Add species
         if species not in self.species:
-            self.add_species(name=species)
+            self.add_species(name=species, species_map=species_map)
 
-        self.odes.append(
-            ODE(
-                equation=equation,
-                parameters=converted_params,
-                species=self.species[species],
-                __model__=self,
-            )
+        self.odes[species] = ODE(
+            equation=equation,
+            species=self.species[species],
         )
 
-    def add_species(self, name: str) -> None:
+        self.odes[species].__model__ = self
+
+    def add_species(self, species_string: str = "", **species_map):
+        """Adds single or multiple species to a model. If multiple species are provided as
+        a string where species are separated by a comma, the species will be added one by one.
+        """
+
+        if not all(isinstance(value, str) for value in species_map.values()):
+            raise TypeError("Species names must be of type str")
+
+        if species_string:
+            species_map.update(
+                {str(species): str(species) for species in symbols(species_string)}
+            )
+
+        for symbol, name in species_map.items():
+            if not isinstance(name, str):
+                raise TypeError("Species names must be of type str")
+
+            self.species[symbol] = Species(name=name, symbol=Symbol(symbol))
+
+    def _add_single_species(
+        self, name: str, species_map: Optional[Dict[str, str]] = None
+    ) -> None:
         """Adds a species to a model"""
 
-        symbol = symbols(name)
+        if species_map is None:
+            self.species[name] = Species(name=name, symbol=symbols(name))
+            return
 
-        if isinstance(symbol, tuple):
-            self.species.update({str(sym): Species(name=sym) for sym in symbol})
+        if self._is_symbol(name, species_map):
+            symbol = name
+            name = species_map[str(symbol)]
         else:
-            self.species[str(symbol)] = Species(name=symbol)
+            symbol = self._gather_symbol_from_species_map(name, species_map)
+
+        self.species[str(symbol)] = Species(name=name, symbol=Symbol(symbol))
+
+    @staticmethod
+    def _is_symbol(name: str, species_map: Dict[str, str]) -> bool:
+        """Checks whether the given name is an identifer (key of species_map)"""
+
+        inverse_dict = {v: k for k, v in species_map.items()}
+
+        if str(name) not in species_map and name not in inverse_dict:
+            raise ValueError(f"Species {name} not found in species map")
+
+        return name in species_map
+
+    @staticmethod
+    def _gather_symbol_from_species_map(name: str, species_map: Dict[str, str]) -> str:
+        """Converts a name to a sympy symbol"""
+
+        inverse_dict = {v: k for k, v in species_map.items()}
+
+        if name not in inverse_dict:
+            raise ValueError(f"Species {name} not found in species map")
+
+        return str(inverse_dict[name])
 
     def add_parameter(
         self,
@@ -169,7 +237,7 @@ class Model(BaseModel):
     def __repr__(self):
         """Prints a summary of the model"""
 
-        for ode in self.odes:
+        for ode in self.odes.values():
             odeprint(y=ode.species.name, expr=ode.equation)
 
         return ""
