2b1a5d87a05d84b95ed50e4a5be48ac7a3ed94fb
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-05-02 23:38:27 +0200

    Add defaults and return to vmapped term

--- sysbiojax/tools/simulation.py
+++ sysbiojax/tools/simulation.py
@@ -3,7 +3,7 @@ from typing import Dict, List
 import equinox as eqx
 import jax
 import jax.numpy as jnp
-from diffrax import ODETerm, PIDController, SaveAt, diffeqsolve
+from diffrax import ODETerm, PIDController, SaveAt, diffeqsolve, Kvaerno5
 from jax import Array
 
 
@@ -11,12 +11,13 @@ class Stack(eqx.Module):
     modules: List[eqx.Module]
 
     def __call__(self, t, y, args):
-        maps, parameters, constants = args
+        species_maps, parameter_maps, parameters = args
 
-        ys = {symbol: y[..., i] for symbol, i in maps.items()}
+        ys = {symbol: y[..., i] for symbol, i in species_maps.items()}
+        params = {symbol: parameters[i] for symbol, i in parameter_maps.items()}
 
         return jnp.stack(
-            [module(**ys, **parameters, **constants) for module in self.modules],  # type: ignore
+            [module(**ys, **params) for module in self.modules],  # type: ignore
             axis=-1,
         )
 
@@ -27,30 +28,25 @@ def simulate(
     t0: int,
     t1: int,
     dt0: float,
-    solver,
-    parameters: Dict[str, float],
-    maps: Dict[str, int],
-    constants: Dict[str, float],
+    parameters: Array,
+    parameter_maps: Dict[str, int],
+    species_maps: Dict[str, int],
     saveat: SaveAt,
-    stepsize_controller: PIDController,
+    stepsize_controller: PIDController = PIDController(rtol=1e-5, atol=1e-5),
+    solver=Kvaerno5(),
 ):
     """Simulates a given model"""
 
-    def solve(y0):
-        sol = diffeqsolve(
-            term,
-            solver,
-            t0=t0,
-            t1=t1,
-            dt0=dt0,
-            y0=y0,
-            args=(maps, parameters, constants),
-            saveat=saveat,
-            stepsize_controller=stepsize_controller,
-        )
+    sol = diffeqsolve(
+        term,
+        solver,
+        t0=t0,
+        t1=t1,
+        dt0=dt0,
+        y0=y0,
+        args=(species_maps, parameter_maps, parameters),
+        saveat=saveat,
+        stepsize_controller=stepsize_controller,
+    )
 
-        return sol.ts, sol.ys
-
-    ts, ys = jax.vmap(solve)(y0)
-
-    return ts, ys
+    return sol.ts, sol.ys
