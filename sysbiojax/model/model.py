e46de9e92b7508e962fcb9d2c3b7a0cf55d1a8ee
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-05-02 21:32:19 +0200

    Revert "Changed vmap to term"
    This reverts commit 35eb62df46ff28930ee2c49c1df6e21e114f7d37.

--- sysbiojax/model/model.py
+++ sysbiojax/model/model.py
@@ -318,15 +318,18 @@ class Model(BaseModel):
     def __repr__(self):
         """Prints a summary of the model"""
 
+        print("# Species", end="\n\n")
         eqprint(
             Symbol("x"), Matrix([species.symbol for species in self.species.values()]).T
         )
 
+        print("\n# Parameters", end="\n\n")
         eqprint(
             Symbol("theta"),
             Matrix([param.name for param in self.parameters.values()]).T,
         )
 
+        print("\n# Equations", end="\n\n")
         for ode in self.odes.values():
             odeprint(y=ode.species.name, expr=ode.equation)
 
