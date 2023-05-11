01ab340da6b8982adc77ed88d0f54d22d75280f3
haeussma <83341109+haeussma@users.noreply.github.com>
2023-05-11 22:51:52 +0200

    analysis class extract data method

--- sysbiojax/analysis/analysis.py
+++ sysbiojax/analysis/analysis.py
@@ -1,5 +1,8 @@
 from typing import Dict, List
 from pydantic import BaseModel
+import jax
+import jax.numpy as jnp
+import numpy as np
 
 from sysbiojax.model.model import Model
 from sysbiojax.measurement.measurment import Experiment
@@ -7,17 +10,49 @@ from sysbiojax.measurement.measurment import Experiment
 
 class Analysis(BaseModel):
     data: Experiment
-    model: Model
+    model: Model = None
 
-    def _initialize_measurement_data(self):
+    def __post_init__(self):
+        self.data_array = self._initialize_data_matrix()
+
+    def _get_dimentions(self):
+        """Returns dimentions of the experiment as needed for the simulate funcion of `Model`.
+        shape = (measurements, species, repetitions, time)
+        """
+
         n_measurements = len(self.data.measurements)
 
-        reactant_keys = sorted(
-            [measurement.reactants.keys() for measurement in self.data.measurements]
-        )
-        n_reactants = len(reactant_keys)
+        species_keys = [
+            list(measurement.reactants.keys()) for measurement in self.data.measurements
+        ]
+        unique_species = sorted(list({x for l in species_keys for x in l}))
+        n_species = len(unique_species)
 
-        for measurement in self.data.measurements:
-            for key in reactant_keys:
-                if measurement.reactants[key].data != None:
-                    a
+        n_replicates = max(
+            measurement.get_max_replicates() for measurement in self.data.measurements
+        )
+        max_data_length = max(
+            measurement.get_max_data_lengths() for measurement in self.data.measurements
+        )
+
+        n_measurements = len(self.data.measurements)
+
+        return n_measurements, n_species, n_replicates, max_data_length
+
+    def _initialize_data_matrix(self) -> jax.Array:
+        """Initializes an array with the dimentions (measurements, species, repetitions, time),
+        containing measurement data from 'data'.
+        Array is initialized with nan values. Then, 'data' is inserted in the right dimension.
+        Species are ordered alphabetically"""
+        matrix = np.empty(self._get_dimentions())
+        matrix[:] = np.nan
+
+        for m, measurement in enumerate(self.data.measurements):
+            for s, species_id in enumerate(sorted(measurement.reactants.keys())):
+                if measurement.reactants[species_id].data is not None:
+                    for r, replicate in enumerate(
+                        measurement.reactants[species_id].data
+                    ):
+                        matrix[m, s, r] = replicate.values
+
+        return jnp.array(matrix)
