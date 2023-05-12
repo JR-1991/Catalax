f8e3e02059464cc12cebf04214ef9718d474c772
haeussma <83341109+haeussma@users.noreply.github.com>
2023-05-12 18:11:16 +0200

    working parameter estimator

--- sysbiojax/tools/parameterestimation.py
+++ sysbiojax/tools/parameterestimation.py
@@ -1,30 +1,73 @@
 from typing import Dict, List
 from pydantic import BaseModel, PrivateAttr
+from lmfit import Parameters, minimize
 import jax
+import jax.numpy as jnp
 
 from sysbiojax.model.model import Model
 
 
-class Simulation(BaseModel):
-    class Config:
-        arbitrary_types_allowed = True
+class ParameterEstimator:
+    def __init__(
+        self,
+        model: Model,
+        data: jax.Array,
+        time: jax.Array,
+        initial_states: List[Dict[str, float]],
+    ):
+        self.model = model
+        self.data = data
+        self.time = time
+        self.initial_states = initial_states  # from data
 
-    term = 0
-    dt0: float
+        self.lmfit_params = self._init_lmfit_params()
 
-    _simulation_func = PrivateAttr(default=None)
+    def _init_lmfit_params(self) -> Parameters:
+        """Creates lmfit Parameters object based on model.parameters."""
+        lmfit_params = Parameters()
 
+        # Get parameters from model
+        param_keys = list(self.model.parameters.keys())
+        initial_params = self.model.parameters
 
-class ParameterEstimator(BaseModel):
-    class Config:
-        arbitrary_types_allowed = True
+        # Map parameters to lmfit 'Parameters'
+        for key in param_keys:
+            lmfit_params.add(
+                name=key,
+                value=initial_params[key].value,
+                min=initial_params[key].lower_bound,
+                max=initial_params[key].upper_bound,
+            )
 
-    model: Model
-    data: jax.Array
-    time: jax.Array
+        return lmfit_params
 
-    def _init_lmfit_params(self):
-        pass
+    def residuals(
+        self,
+        params: Parameters,
+    ):
+        # extract lmfit params and create array for simulation
+        lmfit_params_dict = params.valuesdict()
+        sorted_keys = sorted(list(lmfit_params_dict.keys()))
+        parameters = jnp.array([lmfit_params_dict[key] for key in sorted_keys])
+
+        # simulate
+        _, sim_data = self.model.simulate(
+            initial_conditions=self.initial_states,
+            dt0=0.01,
+            saveat=self.time,
+            parameters=parameters,
+            in_axes=(0, None, 0),
+        )
+
+        # calculate residuals
+        residuals = sim_data - self.data
+
+        return residuals.flatten()
 
     def fit(self):
-        pass
+        """Fits data to model with given initial conditions and parameter guesses."""
+
+        return minimize(
+            fcn=self.residuals,
+            params=self.lmfit_params,
+        )
