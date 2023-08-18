96d151346b5657612a0c98cad836b41614e43ebf
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-08-18 14:55:59 +0200

    Added MCMC to visualisation

--- catalax/tools/visualization.py
+++ catalax/tools/visualization.py
@@ -1,10 +1,12 @@
 import math
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Tuple
 
 import jax
 import jax.numpy as jnp
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
+from numpyro.infer import MCMC
+from numpyro.diagnostics import hpdi
 
 from catalax.model.model import Model
 
@@ -14,6 +16,7 @@ def visualize(
     data: jax.Array,
     times: jax.Array,
     initial_conditions: Optional[List[Dict[str, float]]] = None,
+    mcmc: Optional[MCMC] = None,
     n_cols: int = 2,
     title: Optional[str] = None,
     colors: Optional[Dict] = None,
@@ -21,9 +24,10 @@ def visualize(
     time_unit: str = "s",
     concentration_unit: str = "mM",
     heading_fontsize=12,
-    figsize=(3, 6),
+    figsize: Tuple[int, int] = (4, 2),
     resolution: int = 1000,
-    max_steps=64**4,
+    max_steps: int = 64**4,
+    mass: float = 0.9,
     save: Optional[str] = None,
 ):
     if colors is None:
@@ -48,7 +52,7 @@ def visualize(
             max_steps=max_steps,
         )
     elif has_fit and not initial_conditions:
-        print(f"Cannot plot fit without 'initial_conditions'.")
+        raise ValueError(f"Cannot plot fit without 'initial_conditions'.")
     else:
         times_, states = None, None
 
@@ -62,10 +66,30 @@ def visualize(
 
     _check_color_species_consistency(colors, model)
 
+    if mcmc and initial_conditions and has_fit:
+        # Get boundaries from mcmc
+        lower_quantiles, upper_quantiles = _get_quantile_preds(
+            model=model,
+            mcmc=mcmc,
+            initial_conditions=initial_conditions,
+            time=times_,  # type: ignore
+            mass=mass,
+        )
+
+        has_quantiles = True
+    else:
+        lower_quantiles = None
+        upper_quantiles = None
+        has_quantiles = False
+
     # Initialize column/row iter
     row, col = 0, 0
+    remaining_axes = [(r, c) for r in range(n_rows) for c in range(n_cols)]
 
     for dataset in range(data.shape[0]):
+        # Remove from remaining axes
+        remaining_axes.remove((row, col))
+
         for species, color in colors.items():
             index = model._get_species_order().index(species)
 
@@ -88,6 +112,15 @@ def visualize(
                     c=color,
                 )
 
+            if has_quantiles:
+                ax[row, col].fill_between(
+                    times_[dataset],
+                    lower_quantiles[dataset, :, index],  # type: ignore
+                    upper_quantiles[dataset, :, index],  # type: ignore
+                    alpha=0.3,
+                    color=color,
+                )
+
         if initial_conditions:
             # Add initial conditions as heading
             init_as_string = "  ".join(
@@ -107,16 +140,38 @@ def visualize(
             col = 0
             row += 1
 
-    ax[0, n_cols - 1].legend(loc="center left", bbox_to_anchor=(1, 0.5))
+    legend = ax[0, n_cols - 1].legend(loc="center left", bbox_to_anchor=(1, 0.5))
+
+    # Clean up remaining axes
+    for row, col in remaining_axes:
+        ax[row, col].axis("off")
 
     if title:
-        f.suptitle(title, fontsize=16)
+        bbox_extra_artists = [
+            f.suptitle(
+                title,
+                x=0.05,
+                y=1.05,
+                horizontalalignment="left",
+                verticalalignment="top",
+                fontsize=15,
+            ),
+            legend,
+        ]
+    else:
+        bbox_extra_artists = [legend]
 
     if save:
         if not save.endswith(".png"):
             save = f"{save}.png"
 
-        plt.savefig(save, dpi=300, format="png")
+        plt.savefig(
+            save,
+            dpi=300,
+            format="png",
+            bbox_inches="tight",
+            bbox_extra_artists=bbox_extra_artists,
+        )
 
     return f
 
@@ -138,3 +193,58 @@ def _check_color_species_consistency(
         raise ValueError(
             "Color mapping and model species are not consistent. Please check the color mapping."
         )
+
+
+def _get_quantile_preds(
+    model: Model,
+    mcmc: MCMC,
+    initial_conditions: List[Dict[str, float]],
+    time: jax.Array,
+    mass: float = 0.9,
+) -> Tuple[jax.Array, jax.Array]:
+    samples = mcmc.get_samples()
+    hdpi_range = [
+        hpdi(mcmc.get_samples()[param], mass) for param in model._get_parameter_order()
+    ]
+
+    # Construct upper and lower parameters
+    lower_quantile_params = jnp.array([value[0] for value in hdpi_range])
+    upper_quantile_params = jnp.array([value[1] for value in hdpi_range])
+
+    lower_quantile_pred = _simulate_quantile(
+        model,
+        lower_quantile_params,
+        time,
+        initial_conditions,
+    )
+
+    upper_quantile_pred = _simulate_quantile(
+        model,
+        upper_quantile_params,
+        time,
+        initial_conditions,
+    )
+
+    # Stack both and get the minimum across the stacked dim
+    stacked_quantiles = jnp.stack([lower_quantile_pred, upper_quantile_pred])
+    lower_end = stacked_quantiles.min(axis=0)
+    upper_end = stacked_quantiles.max(axis=0)
+
+    return (lower_end, upper_end)
+
+
+def _simulate_quantile(
+    model: Model,
+    quantile_params: jax.Array,
+    times: jax.Array,
+    initial_conditions: List[Dict[str, float]],
+):
+    # Perform simulation with these parameters
+    _, states = model.simulate(
+        initial_conditions=initial_conditions,
+        saveat=times,
+        in_axes=(0, None, 0),
+        parameters=quantile_params,
+    )
+
+    return states
