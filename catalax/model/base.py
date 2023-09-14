3ab3d2dd3eada3bed2fea5f8d535eccf68d2ac7c
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-09-15 00:52:44 +0400

    Update to enum type

--- catalax/model/base.py
+++ catalax/model/base.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, PrivateAttr
 
 class CatalaxBase(BaseModel):
     class Config:
+        use_enum_values = True
         arbitrary_types_allowed = True
         allow_mutation = True
         validate_assignment = True
