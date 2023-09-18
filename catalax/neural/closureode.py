922e0cca6bd176c1c83d08492968f1b598ffc4f7
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-09-18 06:26:49 +0200

    Added parameter array

--- catalax/neural/closureode.py
+++ catalax/neural/closureode.py
@@ -1,19 +1,20 @@
 from functools import partial
-from typing import Callable, List
+from typing import Callable, List, Optional
 
+import jax
 import diffrax
 import jax.numpy as jnp
 import equinox as eqx
 
 from catalax import Model
+from catalax.tools import Stack
 from .mlp import MLP
 from .neuralbase import NeuralBase
 
 
 class ClosureODE(NeuralBase):
-    vector_field: Callable
-    func: eqx.Module
-    influence: float
+    parameters: jax.Array
+    vector_field: Stack
 
     def __init__(
         self,
@@ -23,10 +24,9 @@ class ClosureODE(NeuralBase):
         model: Model,
         observable_indices: List[int],
         solver=diffrax.Tsit5,
-        influence=1e-2,
         *,
         key,
-        **kwargs
+        **kwargs,
     ):
         super().__init__(
             data_size=data_size,
@@ -39,43 +39,30 @@ class ClosureODE(NeuralBase):
         )
 
         # Save solver and MLP
-        self.influence = influence
-        self.func = MLP(data_size, width_size, depth, key=key)  # type: ignore
+        self.func = MLP(
+            data_size,
+            width_size,
+            depth,
+            key=key,
+        )  # type: ignore
 
         # Get the term of the model and define the closure
-        self.vector_field = self._extract_vector_field(model)
+        self.parameters = jnp.array(model._get_parameters())
+        self.vector_field = eqx.filter_jit(model._setup_rate_function())  # type: ignore
+
+    def _combined_term(self, t, y, args):
+        """Merges neural network terms and vector field"""
+        return self.vector_field(t, y, self.parameters) + self.func(t, y, args)
 
     def __call__(self, ts, y0):
         solution = diffrax.diffeqsolve(
-            diffrax.ODETerm(self._closure),
+            diffrax.ODETerm(self._combined_term),  # type: ignore
             self.solver(),  # type: ignore
             t0=0.0,  # type: ignore
             t1=ts[-1],
             dt0=ts[1] - ts[0],
             y0=y0,
-            stepsize_controller=diffrax.PIDController(rtol=1e-3, atol=1e-6),
-            saveat=diffrax.SaveAt(ts=ts),
+            saveat=diffrax.SaveAt(ts=ts),  # type: ignore
+            max_steps=64**4,
         )
         return solution.ys
-
-    def _closure(self, t, y, args):
-        """Performs closure by adding a neural network term"""
-        return self.vector_field(t, y) + self.influence * self.func(t, y, args)
-
-    @staticmethod
-    def _extract_vector_field(model: Model) -> Callable:
-        """Extracts the vector field from a catalax.Model"""
-        vector_field = model._setup_term().vector_field
-        args = (
-            {species: i for i, species in enumerate(model._get_species_order())},
-            {parameter: i for i, parameter in enumerate(model._get_parameter_order())},
-            jnp.array(
-                [
-                    model.parameters[parameter].value
-                    for parameter in model._get_parameter_order()
-                ]
-            ),
-            model._get_stoich_mat(),
-        )
-
-        return partial(vector_field, args=args)
