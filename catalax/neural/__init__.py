c47b80b6c54649a1096a970dbb605efe2f54f18e
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-09-22 01:44:47 +0200

    Added RBF layer

--- catalax/neural/__init__.py
+++ catalax/neural/__init__.py
@@ -1,4 +1,5 @@
 from .neuralode import NeuralODE
 from .universalode import UniversalODE
 from .trainer import train_neural_ode
+from .rbf import RBFLayer
 from .strategy import Strategy, Step, Modes
