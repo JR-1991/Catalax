eb95e758361966bd1d40a6ed221ac8c5c328b4b7
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-07-06 03:53:52 +0200

    Suppress unused import warning for arviz
    Added '# noqa: F401' to the arviz import to prevent linter warnings about the import being unused.

--- catalax/mcmc/__init__.py
+++ catalax/mcmc/__init__.py
@@ -2,7 +2,7 @@ from .mcmc import run_mcmc, MCMCConfig, BayesianModel, HMC, HMCResults
 from .plotting import plot_corner, plot_posterior, plot_trace, plot_forest, summary
 from . import priors
 
-import arviz as az
+import arviz as az  # noqa: F401
 
 __all__ = [
     "run_mcmc",
