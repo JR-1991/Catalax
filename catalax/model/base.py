2339a7ea774fd85d17213e07be4368fbc0561342
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-07-31 22:56:49 +0200

    Refactor CatalaxBase model_config formatting
    Updated the formatting of the model_config dictionary in CatalaxBase for improved readability and consistency.

--- catalax/model/base.py
+++ catalax/model/base.py
@@ -3,11 +3,10 @@ from pydantic import BaseModel, PrivateAttr, ConfigDict
 
 
 class CatalaxBase(BaseModel):
-
     model_config = ConfigDict(
-        use_enum_values = True,
-        arbitrary_types_allowed = True,
-        validate_assignment = True,
+        use_enum_values=True,
+        arbitrary_types_allowed=True,
+        validate_assignment=True,
     )
 
     _repr_fields: List[str] = PrivateAttr(default=["__all__"])
