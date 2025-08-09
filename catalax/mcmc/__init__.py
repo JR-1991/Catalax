36ca24260e4322af16badf64743f3ff7df224419
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-08-09 02:19:40 +0200

    Expand protocol imports and update __all__ in mcmc
    Added PreModelContext, PostModelContext, pre_model, and post_model to imports and __all__ in catalax/mcmc/__init__.py. Also included models in imports and __all__ for improved module accessibility.

--- catalax/mcmc/__init__.py
+++ catalax/mcmc/__init__.py
@@ -1,7 +1,15 @@
 from .mcmc import run_mcmc, MCMCConfig, BayesianModel, HMC, HMCResults
 from .plotting import plot_corner, plot_posterior, plot_trace, plot_forest, summary
-from .protocols import PreModel, PostModel
+from .protocols import (
+    PreModel,
+    PostModel,
+    PreModelContext,
+    PostModelContext,
+    pre_model,
+    post_model,
+)
 from . import priors
+from . import models
 
 import arviz as az  # noqa: F401
 
@@ -16,10 +24,15 @@ __all__ = [
     "plot_forest",
     "summary",
     "priors",
+    "models",
     "MCMCConfig",
     "BayesianModel",
     "HMC",
     "HMCResults",
     "PreModel",
     "PostModel",
+    "PreModelContext",
+    "PostModelContext",
+    "pre_model",
+    "post_model",
 ]
