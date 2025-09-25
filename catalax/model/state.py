ddb1677ed873caac0f54af3b7287a0578230b2f5
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-09-25 18:33:11 +0200

    Rename Species model to State and update fields
    Renamed the Species class and its file to State, updated the class to include a 'type' field with specific choices, and refactored the symbol validator method name for clarity.

--- catalax/model/state.py
+++ catalax/model/state.py
@@ -1,19 +1,26 @@
-from pydantic import field_validator, ConfigDict
+from typing import Literal, Optional
+
+from pydantic import ConfigDict, field_validator
 from sympy import Expr, symbols
 
 from catalax.model.base import CatalaxBase
 
+STATE_TYPE_CHOICES = Literal[
+    "small_molecule", "protein", "complex", "process_variable", "other"
+]
 
-class Species(CatalaxBase):
+
+class State(CatalaxBase):
     model_config = ConfigDict(
         arbitrary_types_allowed=True,
     )
 
     name: str
     symbol: Expr
+    type: Optional[STATE_TYPE_CHOICES] = None
 
     @field_validator("symbol", mode="before")
-    def convert_string_species_to_sympy(cls, value):
+    def convert_string_state_to_sympy(cls, value):
         """Converts given strings of unit definitions into SymPy symbols"""
 
         if isinstance(value, str):
